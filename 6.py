0680398350219de76a57b54cd63f82cd2914f0dc
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-02-10 09:58:41 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,8 @@
+# Program to input two integer values and calculate first number raised to the power second
+# number.
+a=int(input("enter first number="))
+b=int(input("enter second number="))
+print(pow(a,b))
+# a=int(input("enter first number="))
+# b=int(input("enter second number="))
+# print(a**b)
