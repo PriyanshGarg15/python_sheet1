bcef541dda285bc76b5ed80cf6cd113da1a81a8f
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2023-02-10 09:52:39 +0530

    Update and rename 1. to 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+#Program to print two different strings “Hello” and “World” in different lines.
+a="hello"
+b="world"
+print(a)
+print(b)
+#for same string in different line
+# a='''hello
+# world'''
+# print(a)
